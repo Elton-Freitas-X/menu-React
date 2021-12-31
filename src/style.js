cb69869c4841c95ce6ecd95aa99059386a81068d
Elton Freitas <xelton207@gmail.com>
2021-12-30 23:21:37 -0300

    news

--- src/style.js
+++ src/style.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components'
 
-export const HeaderStyled = styled.div`
-
+export const HeaderStyled = styled.header`
+    width:100%;
+    background-color: ${( {theme }) => theme.primary};
+    z-index: 9999;
+    position: fixed;
+    border-bottom: 1px solid #fff;
 `
 
 export const LogoStyled = styled.div`
-
+    position: absolute;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding-left: 40px;
 `
 
 export const MenuContentStyled = styled.div`
